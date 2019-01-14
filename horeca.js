c9f31371890784b3e4b1c5cc22c1a56657dcfbfe
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2019-01-14 12:38:05 +0100

    finished product, all glitches are goneeee!

--- horeca.js
+++ horeca.js
@@ -1,105 +1,122 @@
-var aantalbier	 = 0;
-var aantalwijn	 = 0;
-var aantalfris	 = 0;
-var bierprijs = 5;
-var wijnprijs = 8;
-var frisprijs = 2;
-var drinkenprijs = 0;
-var bitterballen = 0;
-var bitterbalprijs = 1;
-var iserbier = 0
-var iserwijn = 0
-var iserfris = 0
-var zijnersnacks = 0
+var input = 0;
 
+var fris = 0;
+var wijn = 0;
+var bier = 0;
 
-var drinken = prompt("Welke bestelling wilt u toevoegen?");
+var aantalfris = 0;
+var aantalwijn = 0;
+var aantalbier = 0;
 
+var frisprijs = 2.50;
+var wijnprijs = 5.50;
+var bierprijs = 3.50;
 
-while(drinken != "stop"){																								//zolang je geen 'stop' invoert zal hij de volgende code uitvoeren
-	if(drinken=="bier"){																								//als je bier besteld gaat die deze code uitvoeren
-		var aantalbier = prompt("Hoeveel bier wilt u toevoegen aan uw bestelling?");
-		var drinken = iserbier("wat wilt u drinken?");
-		iserbier = 1																									//als je bier besteld veranderd dit variabel naar 1 zodat de prijs genoteerd kan worden, als het niet besteld wordt zal hij het dus ook niet noteren.
-	}
-	else if(drinken=="wijn"){																							//als je wijn besteld gaat die deze code uitvoeren
-		var aantalwijn = prompt("Hoeveel wijn wilt u toevoegen aan uw bestelling?");
-		var drinken = prompt("wat wilt u drinken?");
-		iserwijn = 1																									//als je wijnbesteld veranderd dit variabel naar 1 zodat de prijs genoteerd kan worden, als het niet besteld wordt zal hij het dus ook niet noteren.
-	}
-	else if(drinken=="fris"){																							//als je fris besteld gaat die deze code uitvoeren
-		var aantalfris = prompt("Hoeveel fris wilt u toevoegen aan uw bestelling?");
-		var drinken = prompt("wat wilt u drinken?");
-		iserfris = 1																									//als je fris besteld veranderd dit variabel naar 1 zodat de prijs genoteerd kan worden, als het niet besteld wordt zal hij het dus ook niet noteren.
-	}
-	else if(drinken != "bier","wijn","fris"){																			//als je geen bier,fris of wijn besteld voert hij deze code uit
-		alert("ik weet niet zeker of dat we dit hebben");
-		var drinken = prompt("wat wilt u drinken?");
-	}
-	else{
-		alert("oke");
-	}
+var kleineschalen = 0;
+var groteschalen = 0;
+
+var kleineschaalprijs = 2.50;
+var groteschaalprijs = 4.50;
+
+var keuze = prompt("wat wilt u drinken?");
+
+while(keuze != 'stop'){
+    if(keuze == "fris"){
+        input = parseInt(prompt("Hoeveel " + keuze + " wilt u bestellen?"));
+        aantalfris= aantalfris + input;
+
+        var keuze = prompt("Wilt u nog wat bestellen?");
+        console.log('aantalfris:'+ aantalfris);
+    }
+    else if(keuze == "wijn"){
+        input = parseInt(prompt("Hoeveel " + keuze + " wilt u bestellen?"));
+        aantalwijn= aantalwijn + input;
+
+        var keuze = prompt("Wilt u nog wat bestellen?");
+        console.log('aantalwijn:'+ aantalwijn);
+    }
+    else if(keuze == "bier"){
+        input = parseInt(prompt("Hoeveel " + keuze + " wilt u bestellen?"));
+        aantalbier= aantalbier + input;
+
+        var keuze = prompt("Wilt u nog wat bestellen?");
+        console.log('aantalbier:'+ aantalbier);
+    }
+    else if(keuze == "snack"){
+        var snacks = prompt("Hoeveel snacks wilt u?","kies uit een schaal van '8' of '16'")
+
+        if(snacks == "8"){
+            alert("successs");
+            var kleineschalen = kleineschalen+1;
+            var keuze = prompt("Wilt u nog wat bestellen?");
+            console.log('kleineschalen:'+ kleineschalen);
+        }
+        else if(snacks == "16"){
+            alert("successs");
+            var groteschalen = groteschalen+1;
+            var keuze = prompt("Wilt u nog wat bestellen?");
+            console.log('groteschalen:'+ groteschalen);
+        }
+        else{
+            alert("dit is een ongeldige invoer");
+            var snacks = prompt("Hoeveel snacks wilt u?","kies uit een schaal ban '8' of '16'")
+        }
+    }
+    else{
+        alert("Ik ben bang dat we geen "+ keuze+ " hebben...");
+        var keuze = prompt("wat wilt u drinken?");
+    }
 }
 
-var totaalbierprijs = aantalbier*bierprijs																				//telt de bestelde drankjes + de prijs op
-var totaalwijnprijs = aantalwijn*wijnprijs																				//telt de bestelde drankjes + de prijs op
-var totaalfrisprijs = aantalfris*frisprijs																				//telt de bestelde drankjes + de prijs op
+var totalefrisprijs = aantalfris*frisprijs;
+var totalewijnprijs = aantalwijn*wijnprijs;
+var totalebierprijs = aantalbier*bierprijs;
 
-var totaalprijsdrinken 	= totaalbierprijs+totaalwijnprijs+totaalfrisprijs												//telt alle prijzen van het drinken bij elkaar op voor de totaalprijs van 't drinken
+var totalekleineschaalprijs = kleineschalen*kleineschaalprijs;
+var totalegroteschaalprijs = groteschalen*groteschaalprijs;
 
-if(iserbier == 1) {
-	document.write("<br>" + aantalbier + "x bier: €" + totaalbierprijs);												//als er bier is besteld gaat die de prijs opschrijven, als dit drankje (bier) niet is besteld noteert hij alleen een <br>
+console.log(totalefrisprijs);
+console.log(totalewijnprijs);
+console.log(totalebierprijs);
+console.log(totalekleineschaalprijs);
+console.log(totalegroteschaalprijs);
+
+var totaalprijs = totalefrisprijs+totalewijnprijs+totalebierprijs+totalekleineschaalprijs+totalegroteschaalprijs;
+
+if(aantalfris == 0){
+    document.getElementById('fris').innerHTML='';
 }else{
-	document.write("<br>")
+    document.getElementById('fris').innerHTML='fris: '+aantalfris+'x, &euro;'+totalefrisprijs+'.';
 }
-if(iserwijn == 1) {
-	document.write("<br>" + aantalwijn + "x wijn: €" + totaalwijnprijs);												//als er wijn is besteld gaat die de prijs opschrijven, als dit drankje (wijn) niet is besteld noteert hij alleen een <br>
+if(aantalwijn == 0){
+    document.getElementById('wijn').innerHTML='';
 }else{
-	document.write("<br>")
+    document.getElementById('wijn').innerHTML='wijn: '+aantalwijn+'x, &euro;'+totalewijnprijs+'.';
 }
-if(iserfris == 1) {
-	document.write("<br>" + aantalfris + "x fris: €" + totaalfrisprijs);												//als er fris is besteld gaat die de prijs opschrijven, als dit drankje (fris) niet is besteld noteert hij alleen een <br>
+if(aantalbier == 0){
+    document.getElementById('bier').innerHTML='';
 }else{
-	document.write("<br>")
+    document.getElementById('bier').innerHTML='bier: '+aantalbier+'x, &euro;'+totalebierprijs+'.';
 }
 
-document.write("<br><br>Totaal prijs voor het drinken: €" + totaalprijsdrinken);										//noteert de totaalprijs van alleen het drinken
-
-
-var eten = prompt("Wilt u nog wat eten?");
-
-
-while(eten != "stop"){																									//zolang je geen 'stop' invoert zal hij de volgende code uitvoeren
-	if(eten == "snack"){
-		var hoeveelsnacks = prompt("Hoeveel bitterballen wilt u toevoegen (8 of 16)?");
-		if(hoeveelsnacks == "8"){																						//als je 8 bitterballen invoert gaat hij de volgende code uitvoeren
-			var achtballen = prompt("hoeveel schalen van 8 bitterballen wilt u?");
-			bitterballen = 8*achtballen+bitterballen
-			var eten = prompt("Wilt u nog wat eten?");
-			var zijnersnacks = 1																						//als je snacks besteld veranderd dit variabel naar 1, zodat die genoteerd kan worden.
-		}
-		else if(hoeveelsnacks == "16"){																					//als je 16 bitterballen invoert gaat hij de volgende code uitvoeren
-			var zestienballen = prompt("hoeveel schalen van 16 bitterballen wilt u?");
-			bitterballen = 16*zestienballen+bitterballen
-			var eten = prompt("Wilt u nog wat eten?");
-			var zijnersnacks = 1																						//als je snacks besteld veranderd dit variabel naar 1, zodat die genoteerd kan worden.
-		}
-		else{
-			alert("u heeft mogelijk een ongeldig getal ingevoerd...");													//foutmelding
-		}
-	}else{
-		alert("ik weet niet zeker of dat we dit hebben")																//foutmelding
-		var eten = prompt("Welke bestelling wilt u toevoegen?");
-	}
+if(kleineschalen == 0){
+    document.getElementById('kleineschaal').innerHTML='';
+}else{
+    document.getElementById('kleineschaal').innerHTML='Kleine totalekleineschaalprijs: '+kleineschalen+'x, &euro;'+totalekleineschaalprijs+'.';
+}
+if(groteschalen == 0){
+    document.getElementById('groteschaal').innerHTML='<br>';
+}else{
+    document.getElementById('groteschaal').innerHTML='Grote totalekleineschaalprijs: '+groteschalen+'x, &euro;'+totalegroteschaalprijs+'.';
 }
 
 
-
-
-totaalbitterbalprijs = bitterballen*bitterbalprijs/4																	//berekent de prijs aan de hand van het aantal bestelde bitterballen
-if(zijnersnacks == 1){																									//als er snacks zijn besteld gaat die deze code uitvoeren
-document.write("<br><br><br>" + bitterballen + " bitterballen = "+ totaalbitterbalprijs + "euro");
-}else{																													//als er geen snacks zijn besteld wordt er alleen een <br> genoteerd
-	document.write("<br>")							
+if(aantalfris == 0,aantalbier == 0, aantalwijn == 0){
+    document.getElementById('fris').innerHTML='U heeft geen drinken besteld<br>';
 }
-document.write("<br><br>Bruto: 	€" + (totaalprijsdrinken+totaalbitterbalprijs) )										//berekent + noteert de totaalrpijs van het drinken en de snacks
+if(kleineschalen == 0,groteschalen == 0){
+    document.getElementById('kleineschaal').innerHTML='U heeft geen snacks besteld<br>';
+}
+
+
+document.getElementById('totaalprijs').innerHTML='totaalprijs: &euro;'+totaalprijs+'.';
